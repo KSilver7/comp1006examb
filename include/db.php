719bafae7e833b8708e3ffc69c8c46dfb0220fdd
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-04-15 08:23:40 -0400

    exam 1

--- include/db.php
+++ include/db.php
@@ -1,3 +1,4 @@
 <?php
+// STEP: 1 db connection
 $db = new PDO('mysql:host=172.31.22.43;dbname=Kristina200213022', 'Kristina200213022', 'n5rj7UsWaG');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
